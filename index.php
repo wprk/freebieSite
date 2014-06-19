a472d194fd013daba5cca53321b36b765b48da9c
Will Parker <wprk14@gmail.com>
2014-06-19 22:28:19 +0100

    testing server name

--- index.php
+++ index.php
@@ -20,6 +20,8 @@ date_default_timezone_set('Europe/London');
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
+die($_SERVER['SERVER_NAME']);
+
 if ($_SERVER['SERVER_NAME']=='freestuffclub.co.uk')
    define('ENVIRONMENT', 'production');
 else if ($_SERVER['SERVER_NAME']=='test.freestuffclub.co.uk')
