34145a401423b9874d2ce643cd809f0cc5228839
Will Parker <wprk14@gmail.com>
2014-06-19 23:54:42 +0100

    Tweaks to get images to upload...

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -645,13 +645,13 @@ class Admin_model extends CI_Model
         $this->load->library('upload', $config);
 
         if ($this->upload->do_upload($field)) {
-            if($this->check_for_image($images, $imgData['img_size'])) {
+            //if($this->check_for_image($images, $imgData['img_size'])) {
                 if($this->db->insert("listing_imgs", $imgData)) {
                     return true;
                 }
-            } else {
-                return $this->update_img($imgData['listing_id'], $field, $imgData);
-            }
+            //} else {
+            //    return $this->update_img($imgData['listing_id'], $field, $imgData);
+            //}
         } else {
             return false;
         }
